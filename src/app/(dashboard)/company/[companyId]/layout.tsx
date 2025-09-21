6f7e77aa1b96bf32bf8ae3f9d0c96966f6396edc
Solarpaletten <167139067+Solarpaletten@users.noreply.github.com>
2025-09-21 15:23:52 +0200

    fix company/[companyId]/layout.tsx

--- src/app/(dashboard)/company/[companyId]/layout.tsx
+++ src/app/(dashboard)/company/[companyId]/layout.tsx
@@ -1,3 +1,17 @@
 import CompanyLayout from './CompanyLayout'
 
-export default CompanyLayout
+export default async function Layout({ 
+  children,
+  params
+}: {
+  children: React.ReactNode
+  params: Promise<{ companyId: string }>
+}) {
+  const { companyId } = await params
+  
+  return (
+    <CompanyLayout companyId={companyId}>
+      {children}
+    </CompanyLayout>
+  )
+}
