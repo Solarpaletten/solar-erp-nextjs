526b5d49bad43e7de59c6ae3562e9ff31988e793
Solarpaletten <167139067+Solarpaletten@users.noreply.github.com>
2025-09-21 15:38:54 +0200

    Remove invalid root layout.tsx file

--- layout.tsx
+++
@@ -1,9 +0,0 @@
-import CompanyLayout from './CompanyLayout'
-
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <CompanyLayout>{children}</CompanyLayout>
-}
