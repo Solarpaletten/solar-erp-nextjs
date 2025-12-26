86c45231a7da131e0ef78a54937ea1b9ec5abbfd
Solarpaletten <167139067+Solarpaletten@users.noreply.github.com>
2025-12-26 23:55:49 +0100

    RESPONSIVE: Sidebar hidden on narrow screens like Site.pro

--- 1/layout.tsx
+++ 1/layout.tsx
@@ -1,4 +1,5 @@
 // src/app/(products)/(dashboard)/company/[companyId]/layout.tsx
+// Responsive Layout - Sidebar hidden on narrow screens like Site.pro
 
 import React from 'react'
 import CompanySidebar from './CompanySidebar'
@@ -15,12 +16,17 @@ export default async function Layout({
   
   return (
     <div className="flex min-h-screen bg-gray-50">
-      {/* Sidebar - скрывается на узких экранах */}
+      {/* ============================================ */}
+      {/* SIDEBAR - Hidden on screens < 1024px (lg:) */}
+      {/* Like Site.pro: sidebar disappears when narrowed */}
+      {/* ============================================ */}
       <div className="hidden lg:block">
         <CompanySidebar companyId={companyId} />
       </div>
       
-      {/* Основной контент */}
+      {/* ============================================ */}
+      {/* MAIN CONTENT - Takes full width when sidebar hidden */}
+      {/* ============================================ */}
       <div className="flex flex-col flex-1 min-w-0">
         <CompanyHeader />
         <main className="flex-1 overflow-x-hidden">
